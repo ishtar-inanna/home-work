bc1ccf55b066ad8e3bff4116241d47bb95e9e439
ishtar-inanna <91423088+ishtar-inanna@users.noreply.github.com>
2021-09-26 21:49:46 +0300

    Add files via upload

--- kursy.1.js
+++ kursy.1.js
@@ -53,26 +53,3 @@ let secondsInMonth = secondsInDay * daysInMonth;
 console.log(`There are ${secondsInMonth} seconds in a Month`);
 
 
-
-
-let x = 1;
-let y = 2;
-
-let res1 =  x + "2"// Допишіть код, необхідно використовувати змінні x і y
-console.log(res1); // "12"
-console.log(typeof res1); // "string"
-
-let res2 =  "true" + 2 // Допишіть код, необхідно використовувати змінні x і y
-console.log(res2); // "true2"
-console.log(typeof res2); // "string"
-
-let res3 =  (x < y) // Допишіть код, необхідно використовувати змінні x і y
-console.log(res3); // true
-console.log(typeof res3); // "boolean"
-
-
-y = "y"
-let res4 = x / y// Допишіть код, необхідно використовувати змінні x і y
-console.log(res4); // NaN
-console.log(typeof res4); // "number"
-
